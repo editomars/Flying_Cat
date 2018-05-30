0347b7ebbb6465e28b19692541fcd74dd20a96e4
editomars <editomars@gmail.com>
2018-05-30 11:43:13 +0000

    adding some key movements

---
+++ Assets/Rocket.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+    Rigidbody rigidBody;
+       
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //check are there any keys down?
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        //ask q. about key that we have pressed
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up); //position in unity is vector 3
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
+
+//bind our keys directly to the thrusters on the rocket.
+//virtual input layout
